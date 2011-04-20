ca20b0d85722c7a5bfc393e3ab3dff78f0e9a393
ilya <ilya@main.lozovyy.com>
2011-04-19 23:20:58 -0400

    Initial Check in of the Credential Management Library

---
+++ CredentialManagement/VistaPrompt.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CredentialManagement
+{
+    public class VistaPrompt: BaseCredentialsPrompt
+    {
+        public VistaPrompt()
+        {
+            Title = "Please provide credentials";
+        }
+        public override bool ShowSaveCheckBox
+        {
+            get
+            {
+                CheckNotDisposed();
+                return 0 != ((int)NativeMethods.WINVISTA_CREDUI_FLAGS.CREDUIWIN_CHECKBOX & DialogFlags);
+            }
+            set
+            {
+                CheckNotDisposed();
+                AddFlag(value, (int)NativeMethods.WINVISTA_CREDUI_FLAGS.CREDUIWIN_CHECKBOX);
+            }
+        }
+
+        public override bool GenericCredentials
+        {
+            get
+            {
+                CheckNotDisposed();
+                return 0 != ((int)NativeMethods.WINVISTA_CREDUI_FLAGS.CREDUIWIN_GENERIC & DialogFlags);
+            }
+            set
+            {
+                CheckNotDisposed();
+                AddFlag(value, (int)NativeMethods.WINVISTA_CREDUI_FLAGS.CREDUIWIN_GENERIC);
+            }
+        }
+
+        public override DialogResult ShowDialog(IntPtr owner)
+        {
+            CheckNotDisposed();
+
+            if (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(Message))
+            {
+                throw new InvalidOperationException("Title or Message should always be set.");
+            }
+
+            uint authPackage = 0;
+            IntPtr outCredBuffer;
+            uint outCredSize;
+            IntPtr inCredBuffer = IntPtr.Zero;
+            int inCredBufferSize = 0;
+
+            bool persist = SaveChecked;
+
+            NativeMethods.CREDUI_INFO credUI = CreateCREDUI_INFO(owner);
+
+            if (!string.IsNullOrEmpty(Username) || !string.IsNullOrEmpty(SecureStringHelper.CreateString(SecurePassword)))
+            {
+                // This seems to be very hacky but don't know a better way to do it yet
+                // Call this method with the same credentials with the empty credentials buffer so that we can get it's size first
+                // but it throws an error because the buffer is too small. So we'll re-initialize the buffer with correct size
+                // and call again to populate the buffer this time.
+                NativeMethods.CredPackAuthenticationBuffer(0, new StringBuilder(Username), new StringBuilder(SecureStringHelper.CreateString(SecurePassword)), inCredBuffer, ref inCredBufferSize);
+                if (Marshal.GetLastWin32Error() == 122)
+                {
+                    // returned from prior method call and we now should have a valid size for the buffer
+                    inCredBuffer = Marshal.AllocCoTaskMem(inCredBufferSize);
+                    if (!NativeMethods.CredPackAuthenticationBuffer(0, new StringBuilder(Username), new StringBuilder(SecureStringHelper.CreateString(SecurePassword)), inCredBuffer, ref inCredBufferSize))
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error(), "There was an issue with the given Username or Password.");
+                    }
+                }
+            }
+
+            //Show the dialog
+            NativeMethods.CredUIReturnCodes dialogResult = NativeMethods.CredUIPromptForWindowsCredentials(ref credUI, ErrorCode, ref authPackage,
+                                                                                inCredBuffer,
+                //You can force that a specific username is shown in the dialog. Create it with 'CredPackAuthenticationBuffer()'. Then, the buffer goes here...
+                                                                                (uint)inCredBufferSize,
+                //...and the size goes here. You also have to add CREDUIWIN_IN_CRED_ONLY to the flags (last argument).
+                                                                                out outCredBuffer,
+                                                                                out outCredSize,
+                                                                                ref persist,
+                                                                                DialogFlags);
+            switch (dialogResult)
+            {
+                case NativeMethods.CredUIReturnCodes.ERROR_CANCELLED:
+                    return DialogResult.Cancel;
+                case NativeMethods.CredUIReturnCodes.ERROR_NO_SUCH_LOGON_SESSION:
+                case NativeMethods.CredUIReturnCodes.ERROR_NOT_FOUND:
+                case NativeMethods.CredUIReturnCodes.ERROR_INVALID_ACCOUNT_NAME:
+                case NativeMethods.CredUIReturnCodes.ERROR_INSUFFICIENT_BUFFER:
+                case NativeMethods.CredUIReturnCodes.ERROR_INVALID_PARAMETER:
+                case NativeMethods.CredUIReturnCodes.ERROR_INVALID_FLAGS:
+                case NativeMethods.CredUIReturnCodes.ERROR_BAD_ARGUMENTS:
+                    throw new InvalidOperationException("Invalid properties were specified.", new Win32Exception(Marshal.GetLastWin32Error()));
+            }
+
+            int maxUsername = 1000;
+            int maxPassword = 1000;
+            int maxDomain = 1000;
+
+            StringBuilder usernameBuffer = new StringBuilder(1000);
+            StringBuilder passwordBuffer = new StringBuilder(1000);
+            StringBuilder domainBuffer = new StringBuilder(1000);
+
+            bool result = NativeMethods.CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize,
+                                                                       usernameBuffer,
+                                                                       ref maxUsername, domainBuffer,
+                                                                       ref maxDomain,
+                                                                       passwordBuffer, ref maxPassword);
+            if (result)
+            {
+                NativeMethods.CoTaskMemFree(outCredBuffer);
+
+                Username = usernameBuffer.ToString();
+                Password = passwordBuffer.ToString();
+
+                if (passwordBuffer.Length > 0)
+                {
+                    passwordBuffer.Remove(0, passwordBuffer.Length);
+                }
+            }
+
+            return DialogResult.OK;
+        }
+    }
+}
